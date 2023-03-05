c17d345c5ed4d1a13818a41aa4136f3986213817
leonasdev <s8952889@gmail.com>
2023-03-05 18:07:04 +0800

    Add Rating component

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      "space-grotesk": ["Space Grotesk", "sans-serif"],
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
